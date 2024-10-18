2b59e0b9981e18a82267d083b34f5cb6244a654f
Professor-Dollar <abdulmoid2007@gmail.com>
2024-10-19 01:56:54 +0500

    initial commit with only 3 pages and currently not fully developed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import FloatingNav from "./components/navigation/FloatingNav";
 import Header from "./components/navigation/Header";
 import Home from "./components/Home/Home";
-import About from './components/about/About';
+import About from './components/About/About';
 import Skills from "./components/skills/Skills";
 
 function App() {
