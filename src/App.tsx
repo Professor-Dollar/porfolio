4d1e547898399457becdd505562ff6b5f5dca4c4
Professor-Dollar <abdulmoid2007@gmail.com>
2024-10-19 01:31:31 +0500

    fixed some deployment bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import FloatingNav from "./components/navigation/FloatingNav";
 import Header from "./components/navigation/Header";
 import Home from "./components/Home/Home";
-import About from "./components/about/About";
-import { useState } from "react";
+import About from './components/about/About';
 import Skills from "./components/skills/Skills";
 
 function App() {
